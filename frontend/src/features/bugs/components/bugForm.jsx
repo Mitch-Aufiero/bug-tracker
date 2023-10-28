23d542c4ed1a23d551faced4182769b49eb5ffa7
Mitch-Aufiero <altermodussoftware@gmail.com>
2023-10-28 12:20:37 -0500

    relational data (projects, users) - mock data and data type conversions

--- frontend/src/features/bugs/components/bugForm.jsx
+++ frontend/src/features/bugs/components/bugForm.jsx
@@ -1,6 +1,19 @@
 import React, { useState } from 'react';
 import { apiRequest } from '../../../api/api';
 
+const mockProjects = [
+    { id: 1, name: 'Project Alpha' },
+    { id: 2, name: 'Project Beta' },
+    
+];
+
+const mockUsers = [
+    { id: 1, name: 'Mortimer Cat\'O Darkness' },
+    { id: 2, name: 'Obi Flamepoint' },
+    
+];
+
+
 function BugForm() {
     const [formData, setFormData] = useState({
         title: '',
@@ -12,6 +25,8 @@ function BugForm() {
         assignedTo: '',
         reportedBy: '',
     });
+    const [projects, setProjects] = useState(mockProjects);
+    const [users, setUsers] = useState(mockUsers);
 
     const handleChange = (event) => {
         setFormData({
@@ -20,16 +35,32 @@ function BugForm() {
         });
     };
 
+    async function usersDropdownPreload(){
+        //call api
+    }
+
     const handleSubmit = (event) => {
         event.preventDefault(); 
         submitBug(formData);
         
     };
 
+    function prepDataForSubmission(data) {
+        const preparedData = {
+            ...data,
+            project: parseInt(data.project, 10), 
+            assignedTo: parseInt(data.assignedTo, 10), 
+            reportedBy: parseInt(data.reportedBy, 10) 
+        };
+    
+    
+        return preparedData;
+    }
+
     async function submitBug(bug) {
         try {
-
-            const data = await apiRequest('/bugs','post',bug);
+            const bugData = prepDataForSubmission(bug);
+            const data = await apiRequest('/bugs','post',bugData);
             //setLoading(false);
 
         } catch (error) {
@@ -72,13 +103,17 @@ function BugForm() {
             </div>
             <div>
                 <label htmlFor="project">Project</label>
-                <input
-                    type="text"
+                <select
+                    type="number"
                     id="project"
                     name="project"
                     value={formData.project}
                     onChange={handleChange}
-                />
+                >
+                    {projects.map(project => (
+                        <option key={project.id} value={project.id}>{project.name}</option>
+                    ))}
+                </select>
             </div>
             <div>
                 <label htmlFor="desc">Description</label>
@@ -102,23 +137,31 @@ function BugForm() {
             </div>
             <div>
                 <label htmlFor="assignedTo">Assigned To</label>
-                <input
-                    type="text"
+                <select
+                    type="number"
                     id="assignedTo"
                     name="assignedTo"
                     value={formData.assignedTo}
                     onChange={handleChange}
-                />
+                >
+                    {users.map(user => (
+                        <option key={user.id} value={user.id}>{user.name}</option>
+                    ))}
+                </select>
             </div>
             <div>
                 <label htmlFor="reportedBy">Reported By</label>
-                <input
-                    type="text"
+                <select
+                    type="number"
                     id="reportedBy"
                     name="reportedBy"
                     value={formData.reportedBy}
                     onChange={handleChange}
-                />
+                >
+                    {users.map(user => (
+                        <option key={user.id} value={user.id}>{user.name}</option>
+                    ))}
+                </select>
             </div>
 
 
