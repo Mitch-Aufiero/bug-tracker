105a97580df4d6e4753574a29aa2f69885cd753f
Mitch-Aufiero <altermodussoftware@gmail.com>
2023-10-31 20:59:06 -0500

    bugForm starting to look satisfactory

---
+++ frontend/src/components/formStyles.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+export const Label = styled.label`
+  margin-bottom: 0.5rem;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  padding: 0.5rem;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+`;
+
+export const Select = styled.select`
+  width: 100%;
+  padding: 0.5rem;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+`;
+
+export const Button = styled.button`
+  padding: 0.25rem;
+  background-image: linear-gradient(144deg,#AF40FF, #5B42F3 50%,#00DDEB);
+  border-radius: 4px;
+  font-size: 19px;
+  color: white;
+  border: 0px solid grey; 
+
+  &:hover {
+    transform: scale(1.05);
+  }
+
+  &:active {
+    transform: translateY(0.135rem); 
+  }
+  
+`;
+
--- frontend/src/features/bugs/components/bugForm.jsx
+++ frontend/src/features/bugs/components/bugForm.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { apiRequest } from '../../../api/api';
-
+import { Label, Input, Select, Button } from '../../../components/formStyles.js'
+import styled from 'styled-components';
 
 const mockUsers = [
     { id: 1, name: 'Mortimer Cat\'O Darkness' },
     { id: 2, name: 'Obi Flamepoint' },
-    
+
 ];
 
 const Statuses = ['New', 'In Progress', 'Resolved', 'Closed'];
@@ -13,6 +14,58 @@ const Severities = ['Low', 'Medium', 'High', 'Critical'];
 const Types = ['Functional Bug', 'Performance Bug', 'UI Bug'];
 
 
+const FormContainer = styled.form`
+    display: grid;
+    height: 100%;
+    color: grey;
+    
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr 1fr;
+    grid-template-areas:
+        "title title title side"
+        "type severity project side"
+        "desc desc desc side";
+    text-align: left;
+    grid-gap: 0.25rem;
+`;
+const TitleSection = styled.div`
+    grid-area: title;
+    padding: 0.25rem;
+    display: flex;
+    flex-direction: column;
+   
+`;
+
+const TypeSection = styled.div`
+    grid-area: type;
+    padding: 0.25rem;
+    display: flex;
+    flex-direction: column;
+`;
+const SeveritySection = styled.div`
+    grid-area: severity;
+    padding: 0.25rem;
+    display: flex;
+    flex-direction: column;
+`;
+const ProjectSection = styled.div`
+    grid-area: project;
+    padding: 0.25rem;
+    display: flex;
+    flex-direction: column;
+`;
+const DescSection = styled.div`
+    grid-area: desc;
+    padding: 0.25rem
+    display: flex;
+    flex-direction: column;
+`;
+const SideSection = styled.div`
+    grid-area: side;
+    padding: 2.25rem;
+    display: flex;
+    flex-direction: column;
+`;
 function BugForm() {
     const [formData, setFormData] = useState({
         title: '',
@@ -25,10 +78,10 @@ function BugForm() {
         reportedBy: '',
     });
     const [users, setUsers] = useState(mockUsers);
-    const [projects, setProjects ] = useState([]);
-    
+    const [projects, setProjects] = useState([]);
+
     useEffect(() => {
-        
+
         fetchProjects();
 
     }, []);
@@ -53,27 +106,27 @@ function BugForm() {
 
 
     const handleSubmit = (event) => {
-        event.preventDefault(); 
+        event.preventDefault();
         submitBug(formData);
-        
+
     };
 
     function prepDataForSubmission(data) {
         const preparedData = {
             ...data,
-            project: parseInt(data.project, 10), 
-            assignedTo: parseInt(data.assignedTo, 10), 
-            reportedBy: parseInt(data.reportedBy, 10) 
+            project: parseInt(data.project, 10),
+            assignedTo: parseInt(data.assignedTo, 10),
+            reportedBy: parseInt(data.reportedBy, 10)
         };
-    
-    
+
+
         return preparedData;
     }
 
     async function submitBug(bug) {
         try {
             const bugData = prepDataForSubmission(bug);
-            const data = await apiRequest('/bugs','post',bugData);
+            const data = await apiRequest('/bugs', 'post', bugData);
             //setLoading(false);
 
         } catch (error) {
@@ -83,33 +136,34 @@ function BugForm() {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="title">Title</label>
-                <input
+
+        <FormContainer onSubmit={handleSubmit}>
+            <TitleSection>
+                <Label htmlFor="title">Title</Label>
+                <Input
                     type="text"
                     id="title"
                     name="title"
                     value={formData.title}
                     onChange={handleChange}
                 />
-            </div>
-            <div>
-                <label htmlFor="type">Bug Type</label>
-                    <select
-                        id="type"
-                        name="type"
-                        value={formData.type}
-                        onChange={handleChange}
-                    >
-                        {Types.map(type => (
-                            <option key={type} value={type}>{type}</option>
-                        ))}
-                    </select>
-            </div>
-            <div>
-                <label htmlFor="severity">Severity</label>
-                <select
+            </TitleSection>
+            <TypeSection>
+                <Label htmlFor="type">Bug Type</Label>
+                <Select
+                    id="type"
+                    name="type"
+                    value={formData.type}
+                    onChange={handleChange}
+                >
+                    {Types.map(type => (
+                        <option key={type} value={type}>{type}</option>
+                    ))}
+                </Select>
+            </TypeSection>
+            <SeveritySection>
+                <Label htmlFor="severity">Severity</Label>
+                <Select
                     id="severity"
                     name="severity"
                     value={formData.severity}
@@ -118,11 +172,11 @@ function BugForm() {
                     {Severities.map(severity => (
                         <option key={severity} value={severity}>{severity}</option>
                     ))}
-                </select>
-            </div>
-            <div>
-                <label htmlFor="project">Project</label>
-                <select
+                </Select>
+            </SeveritySection>
+            <ProjectSection>
+                <Label htmlFor="project">Project</Label>
+                <Select
                     type="number"
                     id="project"
                     name="project"
@@ -132,21 +186,22 @@ function BugForm() {
                     {projects.map(project => (
                         <option key={project.project_id} value={project.project_id}>{project.name}</option>
                     ))}
-                </select>
-            </div>
-            <div>
-                <label htmlFor="description">Description</label>
-                <input
+                </Select>
+            </ProjectSection>
+            <DescSection>
+                <Label htmlFor="description">Description</Label>
+                <Input
                     type="text"
                     id="description"
                     name="description"
                     value={formData.description}
                     onChange={handleChange}
                 />
-            </div>
-            <div>
-                <label htmlFor="status">Status</label>
-                <select
+            </DescSection>
+            <SideSection>
+                <Button type="submit">Submit</Button>
+                <Label htmlFor="status">Status</Label>
+                <Select
                     id="status"
                     name="status"
                     value={formData.status}
@@ -155,11 +210,10 @@ function BugForm() {
                     {Statuses.map(status => (
                         <option key={status} value={status}>{status}</option>
                     ))}
-                </select>
-            </div>
-            <div>
-                <label htmlFor="assignedTo">Assigned To</label>
-                <select
+                </Select>
+
+                <Label htmlFor="assignedTo">Assigned To</Label>
+                <Select
                     type="number"
                     id="assignedTo"
                     name="assignedTo"
@@ -169,11 +223,10 @@ function BugForm() {
                     {users.map(user => (
                         <option key={user.id} value={user.id}>{user.name}</option>
                     ))}
-                </select>
-            </div>
-            <div>
-                <label htmlFor="reportedBy">Reported By</label>
-                <select
+                </Select>
+
+                <Label htmlFor="reportedBy">Reported By</Label>
+                <Select
                     type="number"
                     id="reportedBy"
                     name="reportedBy"
@@ -183,14 +236,10 @@ function BugForm() {
                     {users.map(user => (
                         <option key={user.id} value={user.id}>{user.name}</option>
                     ))}
-                </select>
-            </div>
+                </Select>
+            </SideSection>
 
-
-
-
-            <button type="submit">Submit</button>
-        </form>
+        </FormContainer>
     );
 }
 
